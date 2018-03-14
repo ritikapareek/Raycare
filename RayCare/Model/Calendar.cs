9882ee1581f363f6b6e961cf233696b913f94b67
Ritika Pareek <ritika.pareek@gmail.com>
2018-03-14 15:03:59 -0700

    Adding calendar model

---
+++ RayCare/Model/Calendar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayCare.Model
+{
+    public class Calendar
+    {
+        public const int NUM_DAYS = 180;
+        
+        private int[] days;
+        
+        public Calendar()
+        {
+            this.days = new int[NUM_DAYS];
+        }
+
+        public bool isAvailable(int i)
+        {
+            return days[i] == 0;
+        }
+
+        public void book(int i)
+        {
+            days[i] = 1;
+        }
+
+    }
+}
--- RayCare/Model/Data.cs
+++ RayCare/Model/Data.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using RayCare.Model;
+using System.Collections.Generic;
 
 /// <summary>
 /// Initializes the various data models for Treatment Room, Treatment Machine,
@@ -8,8 +9,13 @@ namespace RayCare
 {
     public class TreatmentRoom
     {
+        public TreatmentRoom()
+        {
+            calender = new Calendar();
+        }
         public string Name { get; set; }
         public string TreatmentMachine { get; set; }
+        public Calendar calender { get; set; }
     }
 
     public class TreatmentMachine
@@ -25,9 +31,14 @@ namespace RayCare
     }
     public class Doctor
     {
+        public Doctor()
+        {
+            calender = new Calendar();
+        }
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Role> Roles { get; set; }
+        public Calendar calender { get; set; }
     }
 
     public enum Role
